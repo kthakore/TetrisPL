b2de805afc7b3d5f2d5d97f3ff43d35b0dc47622
Kartik Thakore <thakore.kartik@gmail.com>
2009-09-15 11:31:07 -0400

    Moved to Class::XSAccessor

--- Tetris.pl
+++ Tetris.pl
@@ -1,24 +1,58 @@
+# warning! Untested code :)
 use strict;
 use warnings;
-use Carp;
 
-sub DIRECTION_UP{ return 0}
-sub DIRECTION_DOWN{ return 1}
-sub DIRECTION_LEFT{ return 2}
-sub DIRECTION_RIGHT{ return 3}
+use Readonly;
 
+# Note to kthatore: if you don't like the $ before the name, 
+# since those are simple values you can just as well do:
+#
+# use constant {
+#   DIRECTION_UP => 0,
+#   DIRECTION_DOWN => 1,
+#   DIRECTION_LEFT => 2,
+#   DIRECTION_RIGHT => 3,
+# };
+#
+# instead of the below:
+
+Readonly my $DIRECTION_UP    => 0;  #rotates blocks
+Readonly my $DIRECTION_DOWN  => 1;  #rotates blocks other way
+Readonly my $DIRECTION_LEFT  => 2;  # move left
+Readonly my $DIRECTION_RIGHT => 3;  # move right
+                                                                                                    
 #Event Super Class
 package Event;
-sub new { my $class = shift; my $self = {}; bless $self, $class; 
-	  $self->name("Generic Event"); return $self  }
-sub name { my $self = shift; $self->{name} = shift if(@_); return $self->{name}}
-
+use Class::XSAccessor
+  constructor => 'new',
+  accessors => {
+     name => 'name',
+  };   
+                                                                                                    
 package Event::Tick;
-sub new { my $class = shift; my $self = {}; bless $self, $class; 
-	  $self->name("CPU Tick Event"); return $self  }
+use base 'Event';
+sub new { 
+  my $class = shift;
+  my $self = $class->SUPER::new( name => 'CPU Tick Event' );
+  return $self;
+}
+                                                                                                    
+package Event::Quit;
+use base 'Event';
+sub new { 
+  my $class = shift;
+  my $self = $class->SUPER::new( name => 'Program Quit Event' );
+  return $self;
+}
 
-package main;
-
+                                                                                                    
+package Event::GridBuiltEvent; #Tetris has a grid
+use base 'Event';
+use Class::XSAccessor
+  accessors => {
+    grid => 'grid',
+  };
+                                                                                                    
+package main; #On the go testing
 my $event = Event::Tick->new;
 print $event->name
-

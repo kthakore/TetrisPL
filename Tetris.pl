87c3526cdc980926bee62f0eed0df96e00708ea8
Kartik Thakore <thakore.kartik@gmail.com>
2009-09-15 10:18:38 -0400

    Event package made

---
+++ Tetris.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use Carp;
+
+sub DIRECTION_UP{ return 0}
+sub DIRECTION_DOWN{ return 1}
+sub DIRECTION_LEFT{ return 2}
+sub DIRECTION_RIGHT{ return 3}
+
+#Event Super Class
+package Event;
+sub new { my $class = shift; my $self = {}; bless $self, $class; 
+	  $self->name("Generic Event"); return $self  }
+sub name { my $self = shift; $self->{name} = shift if(@_); return $self->{name}}
+
+package main;
+
+my $event =Event->new;
+print $event->name
+

8664c51cbc54d03ca49573211b818e234c4c1d17
Kartik Thakore <thakore.kartik@gmail.com>
2009-09-16 08:04:19 -0400

    weaken working?

--- Tetris.pl
+++ Tetris.pl
@@ -161,11 +161,8 @@ sub new{
   my $self = {};
   bless $self, $class;
  if ( defined $_[0] && $_[0]->isa('Event::Manager')) { $self->evt_manager( $_[0] ) } else { die 'Expects an Event::Manager' };
- print Dumper $self;
+ $self->evt_manager->reg_listener($self); 
  my $weak_self = weaken $self;
- print Dumper $weak_self;
- die;
- $self->evt_manager->reg_listener($weak_self); 
  #$self->evt_manager->reg_listener($self); 
  #TODO weaken so it works
  return $self;
